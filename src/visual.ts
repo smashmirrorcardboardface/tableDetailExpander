6cd9e1ed021e824a7eba920bb983fda5a409eab6
SmashMirror <smashmirrorcardboardface@users.noreply.github.com>
2022-02-22 16:38:37 +0000

    initial commit

---
+++ src/visual.ts
@@ -0,0 +1,109 @@
+/*
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+'use strict';
+
+import 'core-js/stable';
+import './../style/visual.less';
+import powerbi from 'powerbi-visuals-api';
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+
+import { v4 as uuidv4 } from 'uuid';
+
+import { VisualSettings } from './settings';
+export class Visual implements IVisual {
+  private target: HTMLElement;
+  private table: HTMLElement;
+  private settings: VisualSettings;
+
+  constructor(options: VisualConstructorOptions) {
+    console.log('Visual constructor', options);
+    this.target = options.element;
+
+    if (document) {
+      this.table = document.createElement('table');
+      this.target.appendChild(this.table);
+    }
+  }
+
+  public update(options: VisualUpdateOptions) {
+    this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+    console.log('Visual update', options);
+
+    let dataViews = options.dataViews;
+    console.log('Testing Data');
+    if (
+      !dataViews ||
+      !dataViews[0] ||
+      !dataViews[0].categorical ||
+      !dataViews[0].categorical.categories ||
+      !dataViews[0].metadata
+    ) {
+      console.log('Incorrect data to draw table.');
+      return;
+    }
+
+    while (this.table.firstChild) {
+      this.table.removeChild(this.table.firstChild);
+    }
+
+    const transformedData = this.transformData(dataViews[0].categorical.categories);
+    console.log('transformedData', transformedData);
+  }
+
+  private transformData(data: powerbi.DataViewCategoryColumn[]) {
+    console.log('data', data);
+
+    let summaryRowColumns = data.filter((c) => c.source.roles.summaryRowColumn);
+    console.log('summaryRowColumns', summaryRowColumns);
+
+    // let detailRowMeta = data.columns.filter((c) => c.roles.detailHTML)[0];
+
+    // let summaryRowData = data.rows.map((row) => {
+    //   return [...row.slice(0, detailRowMeta.index), ...row.slice(detailRowMeta.index + 1)];
+    // });
+  }
+
+  private static parseSettings(dataView: DataView): VisualSettings {
+    return <VisualSettings>VisualSettings.parse(dataView);
+  }
+
+  /**
+   * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
+   * objects and properties you want to expose to the users in the property pane.
+   *
+   */
+  public enumerateObjectInstances(
+    options: EnumerateVisualObjectInstancesOptions
+  ): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+    return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+  }
+}
