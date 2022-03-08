16f14c9a6015cdef038a031772db055a5bab223d
SmashMirror <smashmirrorcardboardface@users.noreply.github.com>
2022-03-08 13:54:30 +0000

    working class react with rsuite table

---
+++ src/dataTransforms.ts
@@ -0,0 +1,51 @@
+export default function transformData(data) {
+  let tableData = [];
+
+  let categories = data.categorical.categories;
+
+  let values = data.categorical.values ? data.categorical.values : null;
+
+  let categoriesAndValues = values ? categories.concat(values) : categories;
+
+  let columnLabels = categoriesAndValues.map(function (cv) {
+    let sortOrder = cv.source.rolesIndex.summaryRowColumn ? cv.source.rolesIndex.summaryRowColumn[0] : null;
+    return { name: cv.source.displayName, sortOrder: sortOrder };
+  });
+
+  let numberOfRecords = getMaxArrayLength(values);
+
+  for (let i = 0; i < numberOfRecords; i++) {
+    let rowData = {};
+    categoriesAndValues.forEach(function (cv) {
+      rowData[cv.source.displayName] = castPrimitiveValue(cv, cv.values[i]);
+    });
+    tableData.push(rowData);
+  }
+
+  let detailRows = categoriesAndValues.filter((c) => c.source.roles.detailHTML);
+
+  let detailValues = detailRows[0].values;
+
+  return {
+    numberOfRecords: numberOfRecords,
+    columnLabels: columnLabels,
+    tableData: tableData,
+    detailValues: detailValues,
+  };
+
+  function castPrimitiveValue(field, value) {
+    let castValue = field?.source.type.dateTime ? new Date(value?.toString()) : value;
+    castValue = castValue instanceof Date ? castValue.toLocaleString() : value;
+    return castValue;
+  }
+
+  function getMaxArrayLength(arrays) {
+    let maxArrayLength = -Infinity;
+    arrays.forEach(function (a, i) {
+      if (a.values.length > maxArrayLength) {
+        maxArrayLength = a.values.length;
+      }
+    });
+    return maxArrayLength;
+  }
+}
--- src/visual.ts
+++ src/visual.ts
@@ -11,8 +11,8 @@ import IViewport = powerbi.IViewport;
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 //import { ReactCircleCard, initialState } from './components/circleCard';
-import { ExpanderTable, updateExpanderTable } from './components/expanderTable';
-
+import { ExpanderTable, initialState } from './components/expanderTable';
+import transformData from './dataTransforms';
 import './../style/visual.less';
 
 export class Visual implements IVisual {
@@ -20,27 +20,39 @@ export class Visual implements IVisual {
   private reactRoot: JSX.Element;
   private viewport: IViewport;
 
+  private tableData = { message: 'initial table' };
+
   constructor(options: VisualConstructorOptions) {
-    this.reactRoot = React.createElement(ExpanderTable, {});
+    this.reactRoot = React.createElement(ExpanderTable, this.tableData as any);
     this.target = options.element;
 
     ReactDOM.render(this.reactRoot, this.target);
   }
 
   public update(options: VisualUpdateOptions) {
+    console.log('Visual update', options);
+
     if (options.dataViews && options.dataViews[0]) {
       const dataView: DataView = options.dataViews[0];
+      const transformedData = transformData(dataView);
+
+      console.log('transformedData', transformedData);
+      const columns = transformedData.columnLabels;
+
+      ExpanderTable.update({
+        columns: columns,
+        rows: transformedData.tableData,
+      });
 
       this.viewport = options.viewport;
       const { width, height } = this.viewport;
       const size = Math.min(width, height);
-      updateExpanderTable({ tableData: { columns: [], data: [] } });
     } else {
       this.clear();
     }
   }
 
   private clear() {
-    //ReactCircleCard.update(initialState);
+    ExpanderTable.update(initialState);
   }
 }
